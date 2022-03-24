dc748a57b4fdb91be6aad8c0e3bce687172a806c
Heloisa <duarteh729@gmail.com>
2022-03-24 16:33:54 -0300

    Customizando as Views

--- MinhaAppMvcCompleta/src/DevIO.App/ViewModels/FornecedorViewModel.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/ViewModels/FornecedorViewModel.cs
@@ -18,10 +18,11 @@ namespace DevIO.App.ViewModels
 
         [DisplayName("Tipo")]
         public int TipoFornecedor { get; set; }
+
         public EnderecoViewModel Endereco { get; set; }
 
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
-        public IEnumerable<ProdutoViewModel> Produtos { get; set; }
+        //public IEnumerable<ProdutoViewModel> Produtos { get; set; }
     }
 }
